7257817fc2fd9415f48704a8b12e7ab446280a8f
Harshal Sakpal <harshalsakpal21@gmail.com>
2024-12-10 17:13:21 +0530

    Add files via upload

--- backend/middleware/Auth.js
+++ backend/middleware/Auth.js
@@ -1,1 +1,23 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config(); // Ensure environment variables are loaded
+
+// Middleware function to verify JWT
+module.exports = function (req, res, next) {
+    // Get token from header
+    const token = req.header('x-auth-token');
+
+    // Check if token is not provided
+    if (!token) {
+        return res.status(401).json({ message: 'No token, authorization denied' });
+    }
+
+    try {
+        // Verify the token
+        const decoded = jwt.verify(token, process.env.JWT_SECRET); // Use the secret from the .env file
+        req.user = decoded.userId; // Attach the user ID to request object
+        next(); // Call the next middleware/route handler
+    } catch (err) {
+        res.status(401).json({ message: 'Token is not valid' });
+    }
+};
 
